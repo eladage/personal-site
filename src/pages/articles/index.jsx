0f8cc7513a1878b8114ccdff09108bbc7d7e14ad
eladage <eladage@gmail.com>
2023-05-16 14:40:27 -0600

    fix weird sizing bug with long url

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -38,10 +38,10 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Eric Ladage</title>
+        <title>Blog - Eric Ladage</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="Collection of writings (if I ever get around to writing anything...)"
         />
       </Head>
       <SimpleLayout
